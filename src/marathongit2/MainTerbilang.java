f870f81d5d74665a69758640c4493e1165b11f7e
jansenkusuma <jansenkusuma@live.com>
2016-08-20 14:43:01 +0700

    Add Feature Terbilang

---
+++ src/marathongit2/MainTerbilang.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package marathongit2;
+
+import marathongit2.Terbilang;
+/**
+ *
+ * @author Jansen Kusuma
+ */
+public class MainTerbilang {
+    public static void main(String[] args){
+        System.out.println(Terbilang.proses(27400));
+    }
+}
